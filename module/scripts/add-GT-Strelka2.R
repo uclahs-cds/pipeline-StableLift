22211198cb76203643c600c950cb83dc0409511c
nkwang <nkwang@mednet.ucla.edu>
2024-09-26 15:11:25 -0700

    add Strelka2 formatting script

---
+++ module/scripts/add-GT-Strelka2.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env Rscript
+# add-GT-Strelka2.R
+####################################################################################################
+#
+# Adds GT to FORMAT fields
+# 0/1 for called variants and ./. for missing data
+#
+####################################################################################################
+
+suppressPackageStartupMessages({
+    library(vcfR);
+    library(data.table);
+    library(argparse);
+    });
+
+###################################################################################################
+# Input
+###################################################################################################
+# Define command line arguments
+parser <- ArgumentParser();
+parser$add_argument('--input-vcf', type = 'character', help = 'Strelka2 VCF to add FORMAT/GT field to');
+args <- parser$parse_args();
+
+# Save command line arguments
+for (arg in names(args)) {
+    assign(gsub('_', '.', arg), args[[arg]]);
+    }
+
+###################################################################################################
+# Main
+###################################################################################################
+vcf <- read.vcfR(input.vcf);
+format.fields <- unlist(strsplit(vcf@gt[1, 'FORMAT'], ':'));
+
+if ('GT' %in% format.fields) {
+    stop('FORMAT/GT field already exists');
+} else {
+    vcf@meta <- append(vcf@meta, '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">', after = grep('##FORMAT', vcf@meta)[1] - 1);
+    vcf@gt[, 'FORMAT'] <- paste0('GT:', vcf@gt[, 'FORMAT']);
+    gt <- vcf@gt[, colnames(vcf@gt) != 'FORMAT'];
+    vcf@gt[, colnames(vcf@gt) != 'FORMAT'] <- ifelse(startsWith(gt, '.'), paste0('./.:', gt), paste0('0/1:', gt));
+    write.vcf(vcf, input.vcf);
+    }
